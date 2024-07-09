37ad50755f092a1678131667be8f0d60f434bae8
nataliia_beliashkina <nata.nata1710@gmail.com>
2024-07-09 19:02:27 +0300

    events folder/weather progress

--- commands/utility/weather.js
+++ commands/utility/weather.js
@@ -1,17 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('weather')
 		.setDescription('Что по погоде'),
 	async execute(interaction) {
-		let weather = ''
-		await fetch('http://api.openweathermap.org/data/2.5/find?q=nizhniy&units=metric&lang=ru&appid=5f66aa2b29ea594b4ba5c6a69978a90c').then((res) => res.json()).then((data) => {
-			weather = data.list.forEach(e => {
-				interaction.reply('Привет пёс, а погодка по Нижнему такая: ' + e.name + ' ' + e.main.temp);
+		await fetch('https://api.openweathermap.org/data/2.5/weather?q=Nizhny%20Novgorod&lang=ru&appid=5f66aa2b29ea594b4ba5c6a69978a90c')
+			.then((resp) => resp.json())
+			.then((data) => {
+				interaction.reply(`
+					> Город ${data.name}
+					> Температура ${Math.round(data.main.temp - 273)}
+					> На улице ${data.weather[0].description}
+				`);
 			});
-		});
-		// console.log(weather)
-		// await interaction.reply('Привет пёс, а погодка по Нижнему такая: ' + weather);
 	},
 };
+
+// https://api.openweathermap.org/data/2.5/forecast?q=Nizhny%20Novgorod&lang=ru&appid=5f66aa2b29ea594b4ba5c6a69978a90c
